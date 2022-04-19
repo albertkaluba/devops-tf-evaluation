635b3f34e6b82ba6a78e796646b459b3f3c75384
Albert Kaluba <>
2022-04-20 00:19:00 +0100

    created lb sg - added sg rule to allow traffic from lb sg to ec2 sg

--- modules/ec2/security-groups.tf
+++ modules/ec2/security-groups.tf
@@ -18,3 +18,32 @@ resource "aws_security_group" "main_sg" {
     Stack_Name       = var.stack_name  
   }
 }
+
+
+resource "aws_security_group" "lb_sg" {
+  name               = "${var.stack_name}-infra-sg"
+  description        = "Allow ssh inbound traffic"
+  vpc_id             = var.vpc_id
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = [var.vpc_cidr_block]
+  }
+
+  tags = {
+    Name             = "${var.stack_name}-infra-sg"
+    Environment      = var.env
+    Stack_Name       = var.stack_name  
+  }
+}
+
+resource "aws_security_group_rule" "egress_lb_to_main_sg" {
+  type                     = "egress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.lb_sg.id
+  source_security_group_id = aws_security_group.main_sg.id
+}
