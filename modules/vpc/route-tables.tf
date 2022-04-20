a23955cb8434c349b5a22f41146bc935c2b128d6
Albert Kaluba <>
2022-04-20 09:14:46 +0100

    added route table association to subnets and default route

--- modules/vpc/route-tables.tf
+++ modules/vpc/route-tables.tf
@@ -1,13 +1,25 @@
-resource "aws_route_table" "example" {
+resource "aws_route_table" "main" {
   vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "${element(var.subnet_cidr_block, count.index)}"
-    gateway_id = aws_internet_gateway.gw.id
-  }
 
   tags = {
     Name            = "${var.stack_name}-infra-rt"
     Environment     = var.env  
   }
+
+  count = 2
+}
+
+resource "aws_route_table_association" "main" {
+  subnet_id      = "${element(aws_subnet.main.*.id, count.index)}"
+  route_table_id = "${element(aws_route_table.main.*.id, count.index)}"
+
+  count = 2
+}
+
+resource "aws_route" "main_default" {
+  route_table_id         = "${element(aws_route_table.main.*.id, count.index)}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+
+  count = 2
 }
